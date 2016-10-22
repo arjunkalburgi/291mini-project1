4726e00150e56ae9aa7d4b304f5eb5a551fc52fd
nulberry <andrea.whittaker95@gmail.com>
2016-10-22 12:10:06 -0600

    Update Staff.py

--- Staff.py
+++ Staff.py
@@ -1,19 +1,27 @@
-import Main
+#create a new staff member
+def new_staff(staff_id, role, name, login, password):
+    
+    insertion = ([staff_id, role, name, login, password])
+    c.execute("INSERT INTO staff VALUES (?)", insertion)
 
-class staff:
+# logs the user out of the system, returns to the login screen    
+def staff_logout():
     
-    def __init__(self, staff_id_, role_, name_, login_):
-        
-        self.staff_id = staff_id_
-        self.role = role_
-        self.name = name_
-        self.login = login_
+    print("Goodbye!")
+    print("-"*10)
     
-    # logs the user out of the system, returns to the login screen    
-    def staff_logout(self):
-        
-        print("Goodbye!")
-        print("-"*10)
-        
-        return
-        # call the login function in main
+    return
+    # call the login function in main
+    
+def get_nurse_tasks():
+    print("Welcome, nurse. Here are the tasks you can perform:")
+    print("-"*10)
+    print("1: List all charts in the system.")
+    print("2: Add a symptom to a patient's chart.")
+    print("3: Create a chart for a patient.")
+    print("4: Close a patient's chart.")
+    key = int(input(print("Enter the number that corresponds to the task (0 to logout): ")))    
+    print("-"*10)
+    
+def n_third_task():
+    
